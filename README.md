38f22822ed193cc3a11b741a3e7e747729993deb
Raymond Yeo <rayyeo.codes@gmail.com>
2021-01-23 18:45:00 -0500

    better repl

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Future Additions:
 1. Take a weighted sum of most recent year's stats, and career stats (80/20)
 2. more error/edge case handling
+
+Web Scraping Help from: https://news.codecademy.com/web-scraping-python-beautiful-soup-mlb-stats/
--- analyzer.py
+++ analyzer.py
@@ -14,10 +14,6 @@ while True:
 csv_data = csv.reader(data)
 data_lines = list(csv_data)
 
-
-player_one = input("Player 1: ").lower()
-player_two = input("Player 2: ").lower()
-
 # Batters
 #['', 'PLAYER', 'YRS', 'G', 'AB', 'R', 'H', '2B', '3B', 'HR', 'RBI', 'BB', 'SO', 'SB', 'CS', 'BA']
 
@@ -26,9 +22,22 @@ player_two = input("Player 2: ").lower()
 
 
 #STEP 2: find player one's statistics + compute
-for i in range(1,len(data_lines)):
-    if data_lines[i][1] == player_one:
-        player_one_data = data_lines[i]
+
+found = False
+while not found:
+    player_one = input("Enter Player 1's name: ").lower()
+    print("Finding and crunching Player 1's stats...")
+    #STEP 2: find player one's statistics + compute
+    for i in range(1,len(data_lines)):
+        if data_lines[i][1] == player_one:
+            player_one_data = data_lines[i]
+            found = True
+            break
+    if found == False:
+        print("Could not find that player. Please re-enter the player's name.")
+
+
+print("Successfully obtained Player 1's data")
 
 if trade_type == "B":
     rbis = float(player_one_data[10])
@@ -42,11 +51,20 @@ else:
     player_one_strength = (80 - 10 * era) + (0.2 * strikeouts) - (0.1 * walks)
 
 #STEP 3: find player two's statistics + compute
-for i in range(1,len(data_lines)):
-    if data_lines[i][1] == player_two:
-        player_two_data = data_lines[i]
 
-#RBIs
+
+found = False
+while not found:
+    player_two = input("Enter Player 2's name: ").lower()
+    print("Finding and crunching Player 2's stats...")
+    for i in range(1,len(data_lines)):
+        if data_lines[i][1] == player_two:
+            player_two_data = data_lines[i]
+            found = True
+            break
+    if found == False:
+        print("Could not find that player. Please re-enter the player's name.")
+
 if trade_type == "B":
     rbis = float(player_two_data[10])
     runs = float(player_two_data[5])
@@ -58,14 +76,16 @@ else:
     walks = float(player_two_data[10])
     player_two_strength = (80 - 10 * era) + (0.2 * strikeouts) - (0.1 * walks)
 
+#compare the players
+
 print(player_one + " has a computed strength of " + str(player_one_strength))
 print(player_two + " has a computed strength of " + str(player_two_strength))
 
 if abs(player_one_strength-player_two_strength) < 10:
-    print("this is a fair trade")
+    print("this is a pretty fair trade")
 
 elif abs(player_one_strength-player_two_strength) > 10 and player_one_strength>player_two_strength:
-    print("this is an unfair trade. " + player_one + " is clearly the better player." )
+    print("this is an unfair trade. " + player_one + " has had a better season." )
 
 elif abs(player_one_strength-player_two_strength) > 10 and player_one_strength < player_two_strength:
-    print("this is an unfair trade. " + player_two + " is clearly the better player." )
+    print("this is an unfair trade. " + player_two + " has had a better season." )
